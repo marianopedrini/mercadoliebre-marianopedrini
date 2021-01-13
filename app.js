7a9f0819c9fd46b19074c2a9049937b796c5c40c
marianopedrini <pedrini.mariano96@gmail.com>
2021-01-12 22:32:30 -0300

    search-bar corregida

--- app.js
+++ app.js
@@ -7,9 +7,17 @@ const publicFolderPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicFolderPath));
 
 app.listen(3000, () => {
-  console.log("Servidor OK corriendo en puerto 3000");
+  console.log("MercadoLiebre corriendo en el puerto 3000");
 });
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/views/login.html");
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(__dirname + "/views/register.html");
 });
